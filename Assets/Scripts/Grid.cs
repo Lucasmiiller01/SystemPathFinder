c5fa31840a87696fff41842c0dff7e261a72973c
Lucas Miiller <lucasmiiller00007@hotmail.com>
2016-09-06 12:19:15 -0300

    System Grid

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class Grid : MonoBehaviour
+{
+	
+	[ SerializeField ] private Transform transform;
+	
+	[ SerializeField ] private Material material;
+	
+	[ SerializeField ] private Vector2 gridSize;
+	
+	[ SerializeField ] private int rows;
+	
+	[ SerializeField ] private int columns;
+
+	void Start()
+	{
+		DrawGrid();
+	}
+	
+
+	public void DrawGrid()
+	{
+		transform.localScale = new Vector3( gridSize.x, gridSize.y, 1.0f );
+		
+		material.SetTextureScale( "_MainTex", new Vector2( columns, rows ) );	
+	}
+	
+}
